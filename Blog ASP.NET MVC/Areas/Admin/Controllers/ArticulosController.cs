0b17a137589f86a6dd5e3a4bc040ac3fcbbc2418
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-12 17:40:57 -0500

    Creando Controlador y index Articulos

---
+++ Blog ASP.NET MVC/Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,35 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.AccesoDatos.Data;
+using BlogCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_ASP.NET_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly ApplicationDbContext _context;
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo, ApplicationDbContext context)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new
+            {
+                data = _contenedorTrabajo.Articulo.GetAll()
+            });
+        }        
+    }
+}
