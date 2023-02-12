5bf1ab3995bc70f531c2e526e66d79b05370d3b0
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-12 15:06:56 -0500

    Agregando método y vista crear categoria
    con vista parcial

--- Blog ASP.NET MVC/Areas/Admin/Controllers/CategoriasController.cs
+++ Blog ASP.NET MVC/Areas/Admin/Controllers/CategoriasController.cs
@@ -23,6 +23,12 @@ namespace Blog_ASP.NET_MVC.Areas.Admin.Controllers
         }
         
         [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new
