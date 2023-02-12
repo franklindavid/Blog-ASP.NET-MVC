2148c33951d4000fcb1dcd85b72d8ccdf7368894
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-12 11:16:41 -0500

    Creando Controllador Categorias

---
+++ Blog ASP.NET MVC/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,34 @@
+﻿using BlogCore.AccesoDatos.Data;
+using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_ASP.NET_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo, ApplicationDbContext context)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new
+            {
+                data = _contenedorTrabajo.Categoria.GetAll()
+            });
+        }
+    }
+}
--- BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
+++ BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
@@ -11,5 +11,7 @@ namespace BlogCore.AccesoDatos.Data.Repository.IRepository
     public interface ICategoriaRepository : IRepository<Categoria>
     {
         IEnumerable<SelectListItem> GetListaCategorias();
+
+        void Update(Categoria categoria);   
     }
 }
