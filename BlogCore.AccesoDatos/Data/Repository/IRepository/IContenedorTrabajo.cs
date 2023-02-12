4e7c9ca3950e8cdcbbb6e0faf64e99b0f85c9fd8
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-12 10:58:43 -0500

    Implementando Contenedor de trabajo

---
+++ BlogCore.AccesoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        ICategoriaRepository Categoria { get; }
+
+        void Save();
+    }
+}
