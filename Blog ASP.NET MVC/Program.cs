1585462f84fe29f6d4246813f0cef7cbf13a65ba
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-12 14:20:28 -0500

    Creando vista categoria
    -agregando contenedor de trabajo a program.cs para que funciones

--- Blog ASP.NET MVC/Program.cs
+++ Blog ASP.NET MVC/Program.cs
@@ -1,4 +1,6 @@
-using Blog_ASP.NET_MVC.Data;
+using BlogCore.AccesoDatos.Data;
+using BlogCore.AccesoDatos.Data.Repository;
+using BlogCore.AccesoDatos.Data.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddScoped<IContenedorTrabajo, ContenedorTrabajo>();
 
 var app = builder.Build();
 
