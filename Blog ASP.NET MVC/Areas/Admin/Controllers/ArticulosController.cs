6e58cf3ce8b323063b5a799243dfc02589b000b2
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-12 21:42:55 -0500

    Implementando metodo Post de editar

--- Blog ASP.NET MVC/Areas/Admin/Controllers/ArticulosController.cs
+++ Blog ASP.NET MVC/Areas/Admin/Controllers/ArticulosController.cs
@@ -3,6 +3,7 @@ using BlogCore.AccesoDatos.Data;
 using BlogCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using BlogCore.Models.ViewModels;
+using NuGet.Packaging.Signing;
 
 namespace Blog_ASP.NET_MVC.Areas.Admin.Controllers
 {
@@ -78,6 +79,55 @@ namespace Blog_ASP.NET_MVC.Areas.Admin.Controllers
                 artivm.Articulo = _contenedorTrabajo.Articulo.Get(id.GetValueOrDefault());
             }
             return View(artivm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ArticuloVM artiVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnviroment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+
+                var articuloDesdeDb = _contenedorTrabajo.Articulo.Get(artiVM.Articulo.Id);
+
+                if (archivos.Count()> 0)
+                {
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+                    var nuevaExtension = Path.GetExtension(archivos[0].FileName);
+
+                    var rutaImagen = Path.Combine(rutaPrincipal, articuloDesdeDb.UrlImagen.TrimStart('\\'));
+                    if(System.IO.File.Exists(rutaImagen))
+                    {
+                        System.IO.File.Delete(rutaImagen);
+                    }
+
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+
+                    artiVM.Articulo.UrlImagen = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    artiVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+
+                    _contenedorTrabajo.Articulo.Update(artiVM.Articulo);
+                    _contenedorTrabajo.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    artiVM.Articulo.UrlImagen =articuloDesdeDb.UrlImagen;
+                }
+                _contenedorTrabajo.Articulo.Update(artiVM.Articulo);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            //artiVM.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(artiVM);
         }
 
         [HttpGet]
