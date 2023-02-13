ad7f7e76fe2944d9f9a126c97166b239b4d31815
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-13 06:59:00 -0500

    Implementando borrar articulo

--- Blog ASP.NET MVC/Areas/Admin/Controllers/ArticulosController.cs
+++ Blog ASP.NET MVC/Areas/Admin/Controllers/ArticulosController.cs
@@ -137,6 +137,38 @@ namespace Blog_ASP.NET_MVC.Areas.Admin.Controllers
             {
                 data = _contenedorTrabajo.Articulo.GetAll()
             });
-        }        
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+
+            var articuloDesdeDb = _contenedorTrabajo.Articulo.Get(id);
+            string rutaDirectorioPrincipal = _hostingEnviroment.WebRootPath;
+            var rutaImagen = Path.Combine(rutaDirectorioPrincipal, articuloDesdeDb.UrlImagen.TrimStart('\\'));
+
+            if (System.IO.File.Exists(rutaImagen))
+            {
+                System.IO.File.Delete(rutaImagen);  
+            }
+
+
+            if (articuloDesdeDb == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Error borrando Articulo"
+                });
+            }
+
+            _contenedorTrabajo.Articulo.Remove(articuloDesdeDb);
+            _contenedorTrabajo.Save();
+            return Json(new
+            {
+                success = true,
+                message = "Articulo borrado correctamente"
+            });
+        }
     }
 }
